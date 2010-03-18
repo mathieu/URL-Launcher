1b701ab2d72aee2f8018ba0c747550cac3e3bcb7
Guillaume Besse <besse@pomme.local>
2010-03-18 11:40:38 +0100

    renamed Untitled.app to Reef4.app

--- main.m
+++ main.m
@@ -1,12 +1,21 @@
 #import <CoreFoundation/CoreFoundation.h>
 #import <Foundation/NSURL.h>
 #import <AppKit/NSWorkspace.h>
+#import <Cocoa/Cocoa.h>
 
 int main (int argc, const char * argv[]) 
 {
-    // insert code here...
-    CFShow(CFSTR("Opening url\n"));
-    NSURL* url = [NSURL URLWithString:@"http://www.cstb.fr"];
-    [[NSWorkspace sharedWorkspace] openURL:url];
+    
+    NSLog(@"Reef4 launcher...");
+	
+	NSBundle* mainBundle = [NSBundle mainBundle];
+	
+	NSString* reef4UrlStr = [mainBundle objectForInfoDictionaryKey:@"Reef4Url"];
+	
+	NSLog(@"Reef4Url=%@", reef4UrlStr);
+	
+	NSURL* url = [NSURL URLWithString: reef4UrlStr];
+    [[NSWorkspace sharedWorkspace] openURL:url]; 
+	
     return 0;
 }
